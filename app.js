25c2c1f7cef639589b9d8d0ecd38687aab8fbe2e
tboyd4 <tboyd1898@gmail.com>
2020-02-14 21:15:42 -0600

    in app.js, manager creation function and engineer function made

--- app.js
+++ app.js
@@ -1,3 +1,60 @@
-const Employee = require('./lib/employee');
+// require statements for classes
+const Manager = require('./lib/manager');
+const Engineer = require('./lib/engineer');
+const Intern = require('./lib/intern');
+const Inquirer = require('inquirer');
+const fs = require('fs');
 
-const employeeTest = new Employee('Tyler', 1, "tyler@email.com");
+// array that will hold our objects (employees)
+let employeeArray = [];
+
+// function that will run initial inquirer prompt. Function will create one manager, and one engineer, the min
+
+const makeManager = function () {
+    // creating the manager
+    Inquirer.prompt([{
+        message: "Manager Name",
+        name: "managername"
+    },
+    {
+        message: "Employee ID",
+        name: "employeeid"
+    },
+    {
+        message: "Manager Email",
+        name: "manageremail"
+    },
+    {
+        message: "Manager Office Number",
+        name: "managerofficenumber"
+    }]).then(function (res) {
+        const manager = new Manager(res.managername, res.employeeid, res.manageremail, res.managerofficenumber);
+        console.log(manager);
+    });
+}
+
+const makeEngineer = function () {
+        // creating an engineer
+        Inquirer.prompt([{
+            message: "Engineer Name",
+            name: "engname"
+        },
+        {
+            message: "Employee ID",
+            name: "employeeid"
+        },
+        {
+            message: "Employee Email",
+            name: "employeeemail"
+        },
+        {
+            message: "Engineer GitHub User Name",
+            name: "enggit"
+        }]).then(function (res) {
+            const engineer = new Engineer(res.engname, res.employeeid, res.employeeemail, res.enggit);
+            console.log(engineer);
+        });
+}
+
+// makeManager();
+makeEngineer();
